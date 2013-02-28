f38bd476f704d05269c19f4519c33be6e9ed59eb
Tyler Davis <tyebud@gmail.com>
2013-02-28 15:16:22 -0500

    Initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JaneslunchCom::Application.config.secret_token = '2a7f5fb8eebd2ba388e47827c51f1374a9336709434484c57c3d537f631592d80eb5f4b6d81274e2d659b69c9d76f2a99f6cd7688b33b9f046282c0ecec179cd'
