8de3ec8e4104350cfb31aae4260ef077468ca778
Tim Hunter <hunternz@gmail.com>
2013-03-02 16:05:12 -0500

    Added validations to group model, and address_for_ordr

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,11 @@ class Group < ActiveRecord::Base
   attr_accessible :name, :addr, :city, :state, :zip, :phone, :addr2
   has_and_belongs_to_many :users
 
+  validates :name, :addr, :city, :presence => true
+  validates :state, :length => { :is => 2 }
+  validates :zip, :length => { :is => 5 }
+  validates :phone, :length => { :is => 10 }
+
   def address_for_ordr
     Ordrin::Data::Address.new(addr, city, state, zip, phone, addr2)
   end
