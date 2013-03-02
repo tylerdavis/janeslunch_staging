0f5b5fb39055959e505fa58188f2d6b6e5e64fe5
Tim Hunter <hunternz@gmail.com>
2013-03-02 14:55:16 -0500

    commit

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,10 @@
 class Group < ActiveRecord::Base
-  attr_accessible :addr, :addr2, :city, :name, :phone, :zip
+
+  attr_accessible :name, :addr, :city, :state, :zip, :phone, :addr2
   has_and_belongs_to_many :users
+
+  def address_for_ordr
+    Ordrin::Data::Address.new(addr, city, state, zip, phone, addr2)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130301220737) do
+ActiveRecord::Schema.define(:version => 20130301225011) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20130301220737) do
     t.string   "addr2"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "state"
   end
 
   create_table "groups_users", :id => false, :force => true do |t|
