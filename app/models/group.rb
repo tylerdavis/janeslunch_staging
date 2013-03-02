1ffa04672380b5c2efae9c2c6c861fa14d409ed4
Tim Hunter <hunternz@gmail.com>
2013-03-02 18:30:50 -0500

    changed seed data, added get restaurants to groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -11,4 +11,10 @@ class Group < ActiveRecord::Base
     Ordrin::Data::Address.new(addr, city, state, zip, phone, addr2)
   end
 
+  def get_restaurant
+    api = Ordrin::APIs.new("D7gdaKOE0qLxhTwFUQ4Pb9NGiz_-etd8J0USSSS_tks", :test)
+    restaurants = api.restaurant.get_delivery_list("ASAP", address_for_ordr)
+  end
+
+
 end
