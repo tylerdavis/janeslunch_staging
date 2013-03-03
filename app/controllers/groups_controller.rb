67e07d690ac46988aa18ba12fd4acf4ec25ff99e
janeeats <jane.vora@gmail.com>
2013-03-03 16:30:13 -0500

    fix for groups controller to be renamed to plural again

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,38 @@
+class GroupsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @group_name = params['groupname']
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    if user_signed_in?
+      @group = Group.new(params[:group])
+      if @group.save
+        redirect_to groups_path, :notice => "You created a new group!"
+      else
+        render "new"
+      end  
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update_attributes(params[:group])
+      redirect_to groups_path, :notice => "You updated your group!"
+    else
+    end
+  end
+end
