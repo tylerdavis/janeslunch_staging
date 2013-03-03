ed5d29d40a388d828e0214cf93fbc2f01e04aa61
Tim Hunter <hunternz@gmail.com>
2013-03-03 13:43:13 -0500

    Add GroupOrder model and logic to auto select restaurant when new GroupOrder created.

--- app/models/group_order.rb
+++ app/models/group_order.rb
@@ -1,19 +1,18 @@
 class GroupOrder < ActiveRecord::Base
   attr_accessible :delivery_time, :group_id, :ordr_rid
+  # attr_accessor :ordr_rid
   belongs_to :group
   before_save :get_restaurant
 
   API_KEY = "D7gdaKOE0qLxhTwFUQ4Pb9NGiz_-etd8J0USSSS_tks"
 
-  ordr_rid=11
-
   def get_restaurant
-    puts "YEAH"
-    # api = Ordrin::APIs.new(API_KEY, :test)
-    # restaurants = api.restaurant.get_delivery_list("ASAP", self.group.address_for_ordr)
-    # restaurants.scramble
-    ordr_rid = 10
-    delivery_time = Time.now
+    
+    api = Ordrin::APIs.new(API_KEY, :test)
+    address = self.group.address_for_ordr
+    restaurants = api.restaurant.get_delivery_list("ASAP", address)
+    restaurants.shuffle
+    self.ordr_rid = restaurants[0] || 0
   end
 
 
