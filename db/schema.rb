4429d1ed80448c2e9d7233e1d57cf236464d9999
janeeats <jane.vora@gmail.com>
2013-03-03 17:12:12 -0500

    change datatype of lunchtime column to time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130304204310) do
+ActiveRecord::Schema.define(:version => 20130303214227) do
 
   create_table "choices", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(:version => 20130304204310) do
     t.string   "addr2"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.time     "lunch_time"
     t.string   "state"
-    t.datetime "lunch_time"
   end
 
   create_table "groups_users", :id => false, :force => true do |t|
