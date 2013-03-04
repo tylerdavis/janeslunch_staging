56acb9c2b1f663be523828fc9850cc65bdc3e40a
Tim Hunter <hunternz@gmail.com>
2013-03-04 16:29:50 -0500

    Prevented error when no restaurant can be found

--- app/models/group_order.rb
+++ app/models/group_order.rb
@@ -8,12 +8,17 @@ class GroupOrder < ActiveRecord::Base
   scope :from_today, where(" created_at between ? AND ?", Time.now.beginning_of_day, Time.now.end_of_day)
 
   def get_restaurant
+    #@TODO - working with everything being Eastern time at the moment. Need to take different timezones into account!
     time = self.group.lunch_time
     time = DateTime.parse(time + ' Eastern')
     api = Ordrin::APIs.new(API_KEY, :test)
     address = self.group.address_for_ordr
     restaurants = api.restaurant.get_delivery_list(time, address)
-    self.ordr_rid = restaurants.sample['id'] || 0
+    begin
+      self.ordr_rid = restaurants.sample['id']
+    rescue
+      self.ordr_rid = 0
+    end
   end
 
 end
