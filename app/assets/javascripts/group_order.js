50ff6d9e4a4033cc67cc4bbb52adc47e6acdc888
Tyler Davis <tyebud@gmail.com>
2013-03-06 17:41:53 -0500

    modal stuff - tip calculator

--- app/assets/javascripts/group_order.js
+++ app/assets/javascripts/group_order.js
@@ -13,12 +13,24 @@ $(function() {
   });
 
   $('#pop-modal').click(function(event) {
-    // $.getJSON('/items/'+ item + '.json', function(data) {
-    $.getJSON('/items/88.json', function(data) {
+    $.getJSON('/items/'+ item + '.json', function(data) {
+    // $.getJSON('/items/88.json', function(data) {
       $('#myModalLabel').html(data.name);
 
+      var container = $('#option-column');
+      container.html('');
+
       for (var i = 0; i < data.options.length; i++) {
-        console.log(data.options[i].name);
+
+        var option = $('<div class="option"></div>');
+        var optionLabel = $('<legend>' + data.options[i].name + '</legend>');
+        option.append(optionLabel);
+        for (var j = 0; j < data.options[i].suboptions.length; j++) {
+          var price = data.options[i].suboptions[j].price === 0 ? '' : ' - <em>+$' + data.options[i].suboptions[j].price + '</em>';
+          var suboption = $('<div class="suboption"><label class="checkbox"><input type="checkbox">' + data.options[i].suboptions[j].name + price + '</label></div>');
+          option.append(suboption);
+        }
+        container.append(option);
       }
     })
     .success(function() { console.log("Item request success"); })
