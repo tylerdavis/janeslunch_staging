8c02a29c924621cf28b0a66507be185b74198de9
Tim Hunter <hunternz@gmail.com>
2013-03-07 11:26:06 -0500

    reverse change to migration as tyler already did it

--- db/migrate/20130307152351_item_group_order_table.rb
+++ db/migrate/20130307152351_item_group_order_table.rb
@@ -1,9 +1,8 @@
 class ItemGroupOrderTable < ActiveRecord::Migration
   def change
-    create_table :group_order_items do |t|
+    create_table :group_orders_items do |t|
       t.integer :group_order_id
       t.integer :item_id
-      t.timestamps
     end
   end
 end
